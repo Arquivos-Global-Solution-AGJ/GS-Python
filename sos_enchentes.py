de31ccc307bff743483f7b131e83e6f6fd02c61a
glMuller <glmuller2001@gmail.com>
2025-05-27 09:03:51 -0300

    criada função me_proteger, onde exibe informações e dicas para o usuário se proteger de inundações.

--- sos_enchentes.py
+++ sos_enchentes.py
@@ -1,12 +1,12 @@
 def menu_principal():
     while True:
-
-        print("(1) Minha área corre risco de inundar? \n(2) Como posso me protejer de possíveis inundações? \n(3) Agora está perigoso? \n(4) Precisa de ajuda? contate-nos IMEDIATAMENTE \n(5) Sair")
+        print("---Bem vindo(a) ao sistema do SOS ENCHENTES---")
+        print("(1) Minha área corre risco de inundar? \n(2) Como posso me proteger de possíveis inundações? \n(3) Agora está perigoso? \n(4) Precisa de ajuda? contate-nos IMEDIATAMENTE \n(5) Sair")
         escolha = int(input())
         if escolha == 1:
             minha_area()
         elif escolha == 2:
-            me_protejer()
+            me_proteger()
         elif escolha == 3:
             situacao_atual()
         elif escolha == 4:
@@ -25,4 +25,6 @@ def minha_area():
     else:
         print("Essa área não é considerada de fácil alagamento, mas pode ocorrer, fique atento!\n\n")
 
-##Função situacao_atual
+##Função me_proteger
+def me_proteger():
+    print("Se você escolheu essa opção. Fique atento as instruções \n ANTES DA INUNDAÇÃO \n1º Monitore alertas meteorológicos\n2º Tenha um plano de evacuação – Saiba quais rotas usar e onde estão os abrigos mais próximos.\n3º Monte um kit de emergência – Inclua água potável, alimentos não perecíveis, lanternas, pilhas, rádio, documentos importantes, remédios, roupas extras e itens de higiene.\n4º Desconecte aparelhos elétricos – Evite riscos de curtos-circuitos ou choques.\n5º Avalie a estrutura da sua casa – Se for uma área de risco, considere sair antes do alagamento.\n\n ⚠️DURANTE A INUNDAÇÃO⚠️\n -Mantenha a calma e acione a Defesa Civil (199) ou Corpo de Bombeiros (193).\n -Saia do local se as autoridades pedirem.\n -Nunca tente atravessar áreas alagadas a pé ou de carro. \n\n DEPOIS DA INUNDAÇÃO \n -Espere autorização das autoridades para retornar ao imóvel.\n -Evite contato com a água residual.\nAtualize seus documentos e registre os danos, caso precise de apoio do governo ou seguro.\n\n Essas instruções são conferidas por especialistas, proteja-se, a natureza não brinca!")
